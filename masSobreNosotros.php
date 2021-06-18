6ec94817018a765e4fb41c5e67faea441e495c75
cmr718 <48754093+cmr718@users.noreply.github.com>
2021-06-18 11:57:07 +0200

    Update masSobreNosotros.php

--- masSobreNosotros.php
+++ masSobreNosotros.php
@@ -6,31 +6,23 @@
 	<title>Darprint Creations</title>
 	<link rel="stylesheet" href="estiloCabecera.css">
 	<link rel="stylesheet" href="normalize.css">
-	<link rel="stylesheet" href="estiloMasSobreNosotros.css">
+	<link rel="stylesheet" href="estiloMasSobreNosotrosxd.css">
 </head>
 
 <body>
 	<header class="header">
-		<a id="logo" title="logoEmpresa" href="pagprincipal.php" class="logo"><img
-				src="imagenes/logo.png" width="230"
-				height="85" /></a>
+		<a id="logo" title="logoEmpresa" href="pagprincipal.php" class="logo"><img src="http://localhost/TW/imagenes/logo.png" width="230" height="85" /></a>
 		<div class="arriba">
-			<a title="Telefono" class="telefono"><img
-					src="imagenes/telefono.png"
-					width="30" height="30" /> 667 52 28 49</a>
-			<a title="Carrito" class="carrito" href="permisosCesta.php"><img
-					src="imagenes/carrito.png"
-					alt="Carrito" width="30" height="30" /></a>
-			<a title="Usuario" class="usuario" href="permisos.php"><img
-					src="imagenes/usuario.png"
-					alt="Usuario" width="30" height="30" /></a>
+			<a title="Telefono" class="telefono"><img src="http://localhost/TW/imagenes/telefono.png" width="30" height="30" /> 667 52 28 49</a>
+			<a title="Carrito" class="carrito" href="cestaAdmin.php"><img src="http://localhost/TW/imagenes/carrito.png" alt="Carrito" width="30" height="30" /></a>
+			<a title="Usuario" class="usuario" href="permisos.php"><img src="http://localhost/TW/imagenes/usuario.png" alt="Usuario" width="30" height="30" /></a>
 		</div>
 		<nav class="navigation">
 			<ul>
 				<li><a href="pagprincipal.php">INICIO</a></li>
-                <li><a href="Catalogo.php">CATÁLOGO</a></li>
-                <li><a href="personalizaTuProducto.php">PERSONALIZA TU PRODUCTO</a></li>
-                <li><a href="masSobreNosotros.php">MÁS SOBRE NOSOTROS</a></li>
+				<li><a href="Catalogo.php">CATÁLOGO</a></li>
+				<li><a href="personalizaTuProducto.php">PERSONALIZA TU PRODUCTO</a></li>
+				<li><a href="masSobreNosotros.php">MÁS SOBRE NOSOTROS</a></li>
 
 			</ul>
 		</nav>
@@ -40,89 +32,88 @@
 	<main class="main">
 		<div class="container">
 			<strong><a>MAS SOBRE NOSOTROS</a></strong>
-		</div>
-
-		<div class="titulo">
-			<p>NUESTRAS REDES SOCIALES:</p>
-		</div>
-
-		<div class="cuerpo">
-			<a>
-				<p>Puedes seguirnos en nuestras redes sociales para estar mas informado sobre nuestros diseños y
-					ofertas, o para contactar con nosotros si tiene alguna pregunta.</p>
-			</a>
-			<div class="imagenes">
-				<div class="logosSocial">
-					<a title="Instagram" class="insta"><img
-							src="imagenes/instagram.png"></a>
-				</div>
-				<div class="wha">
-					<a title="Whatsapp" class="what"><img
-							src="imagenes/whatsapp.png"></a>
-				</div>
-				<div class="fac">
-					<a title="Facebook" class="face"><img
-							src="imagenes/facebook.png"></a>
-				</div>
-			</div>
-			<div class="desc">
-				<div>
-					<p>@darprintcreations</p>
-				</div>
-				<div>
-					<p>667 52 28 49</p>
-				</div>
-				<div>
-					<p>Darprint Creations</p>
-				</div>
+			<div class="social">
+				<a><img src="http://localhost/TW/imagenes/social.png" /></a>
 			</div>
 		</div>
 
-		<div class="correoytelf">
-			<p>CORREO Y TELEFONO DE CONTACTO:</p>
-		</div>
-
-		<div class="cuerpo2">
-			<a>
-				<p>Si desea contactar de forma mas directa</p>
-			</a>
-			<div class="imagenes2">
-				<div class="logosSocial2">
-					<a title="correo" class="correo"><img
-							src="imagenes/correo.png"></a>
-				</div>
-				<div class="llama">
-					<a title="Llamada" class="llamada"><img
-							src="imagenes/telefono.png"></a>
-				</div>
-			</div>
-			<div class="desc2">
-				<div>
-					<p>sanay_15@hotmail.com</p>
-				</div>
-				<div>
-					<p>667 52 28 49</p>
-				</div>
-			</div>
-		</div>
-
-		<div class="encuentranos">
-			<p>ENCUENTRANOS EN NUESTRA TIENDA FISICA</p>
-		</div>
 
 		<div class="cuerpo3">
-			<div class="imagenes3">
-				<div class="ubi">
-					<a title="Ubicacion" class="ubicacion"><img
-							src="imagenes/mapa.png"></a>
+			<div class="ubicacion">
+				<div class="encuentranos">
+					<p>ENCUENTRANOS EN NUESTRA TIENDA FISICA</p>
+				</div>
+				<div class="desc3">
+					<div>
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3196.0505994654304!2d-2.8027747844237667!3d36.76935357713455!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd7042c1ca6db655%3A0xa24d118694bde88b!2sAv.%20del%20Oasis%2C%2097%2C%2004700%20El%20Ejido%2C%20Almer%C3%ADa!5e0!3m2!1ses!2ses!4v1624005304659!5m2!1ses!2ses" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+					</div>
 				</div>
 			</div>
-			<div class="desc3">
-				<div>
-					<p>AVN OASIS 97, EL EJIDO (ALMERIA)</p>
+
+
+			<div class="cuerpo">
+
+				<a>
+					<strong>
+						<p>NUESTRAS REDES SOCIALES:</p>
+					</strong>
+				</a>
+
+				<a>
+					<p>Puedes seguirnos en nuestras redes sociales para estar mas informado sobre nuestros diseños y
+						ofertas, o para contactar con nosotros si tiene alguna pregunta.</p>
+				</a>
+				<div class="imagenes">
+					<div class="logosSocial">
+						<a title="Instagram" class="insta"><img src="http://localhost/TW/imagenes/instagram.png"></a>
+					</div>
+					<div class="wha">
+						<a title="Whatsapp" class="what"><img src="http://localhost/TW/imagenes/whatsapp.png"></a>
+					</div>
+					<div class="fac">
+						<a title="Facebook" class="face"><img src="http://localhost/TW/imagenes/facebook.png"></a>
+					</div>
 				</div>
+				<div class="desc">
+					<div>
+						<p>@darprintcreations</p>
+					</div>
+					<div>
+						<p>667 52 28 49</p>
+					</div>
+					<div>
+						<p>Darprint Creations</p>
+					</div>
+				</div>
+				<div class="correoytelf">
+					<strong><p>CORREO Y TELEFONO DE CONTACTO:</p></strong>
+				</div>
+
+				<div class="cuerpo2">
+					<a>
+						<p>Si desea contactar de forma mas directa</p>
+					</a>
+					<div class="imagenes2">
+						<div class="logosSocial2">
+							<a title="correo" class="correo"><img src="http://localhost/TW/imagenes/correo.png"></a>
+						</div>
+						<div class="llama">
+							<a title="Llamada" class="llamada"><img src="http://localhost/TW/imagenes/telefono.png"></a>
+						</div>
+					</div>
+					<div class="desc2">
+						<div>
+							<p>sanay_15@hotmail.com</p>
+						</div>
+						<div>
+							<p>667 52 28 49</p>
+						</div>
+					</div>
+				</div>
+
 			</div>
 		</div>
+
 
 	</main>
 
